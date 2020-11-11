bd0cefb817f2d1253bf05124513f59bccfe2b248
Abdi Mohamud <abdinajibmohamud@gmail.com>
2020-11-11 12:45:06 -0800

    retrying to deploy

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 // Components
@@ -6,10 +6,33 @@ import Product from './Product';
 
 
 const ProductList =({products, addItem}) => {
-	// const { products, addItem };
+	const [search, setSearch] = useState(' ');
+	const onChange = (e) => {
+		setSearch( e.target.value );
+	  };
+	function makeLowerCase(value) {
+		return value.toString().toLowerCase();
+	  };
 	return (
+		<>
+		<div className="searchbar">
+			<label>Search:</label>
+			&nbsp;
+			<input type="text" name="search" value={search} onChange={onChange}/>
+			&nbsp;
+			&nbsp;
+			&nbsp;
+			<label for="cars">Sort By:</label>
+			&nbsp;
+			<select name="cars" >
+			<option value="A-Z">A-Z</option>
+			<option value="priceHighLow">Price (High to Low)</option>
+			<option value="priceLowHigh">Price (Low to High)</option>
+			</select>
+		</div>
 		<div className="products-container">
-			{products.map(product => (
+			{products.filter((item) => item.title.toLowerCase().includes(makeLowerCase(search)))
+					 .map(product => (
 				<Product
 					key={product.id}
 					product={product}
@@ -18,6 +41,7 @@ const ProductList =({products, addItem}) => {
 				 
 			))}
 		</div>
+		</>
 	);
 };
 
